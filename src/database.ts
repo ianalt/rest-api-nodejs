a690a38df6d7e069c08a35d3f1b0c590fdf57b68
Ian Alt <ianalt.dev@gmail.com>
2024-07-24 20:12:47 -0300

    first commit

---
+++ src/database.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import { type Knex, knex as setupKnex } from 'knex'
+import { env } from './env'
+
+export const knexConfig: Knex.Config = {
+	client: env.DATABASE_CLIENT,
+	connection: {
+		filename: env.DATABASE_URL,
+	},
+	useNullAsDefault: true,
+	migrations: {
+		extension: 'ts',
+		directory: './db/migrations',
+	},
+}
+
+export const knex = setupKnex(knexConfig)
